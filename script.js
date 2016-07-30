8d26f427a915d019aa81cbdc99e1d8b3e417cedf
marina <maxamonomaxa@gmail.com>
2016-07-30 17:18:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//date action
+
+
+$(document).ready(function() {
+
+  $(".item>a").magnificPopup({
+    type : 'image',
+    gallery : {
+      enabled : true
+    },
+    // removalDelay: 300,
+    // mainClass: 'mfp-fade'
+});
+  $(".date").effect('bounce', {times:3},500);
+
+  });
+
